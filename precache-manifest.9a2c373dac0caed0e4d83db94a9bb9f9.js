b08404b2b94dd24751cda4cb233d72b935ff7a06
51998138 <alexandersevruk96@gmail.com>
2019-12-03 19:22:11 +0200

    Updates

---
+++ precache-manifest.9a2c373dac0caed0e4d83db94a9bb9f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a943362ab932f663a615b2c689b63006",
+    "url": "/recipebook/index.html"
+  },
+  {
+    "revision": "aa62b201bda17c09e3ce",
+    "url": "/recipebook/static/css/main.99004c8d.chunk.css"
+  },
+  {
+    "revision": "14acd993d2ca4aa76f5d",
+    "url": "/recipebook/static/js/2.c16ea82a.chunk.js"
+  },
+  {
+    "revision": "aa62b201bda17c09e3ce",
+    "url": "/recipebook/static/js/main.e3b3885d.chunk.js"
+  },
+  {
+    "revision": "c2616c3c417d718b8f81",
+    "url": "/recipebook/static/js/runtime-main.3e5145d8.js"
+  }
+]);
